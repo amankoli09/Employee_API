75e17a524a60dbb77af4bd83742897c92a50298d
Aman koli <2025.amana@isu.ac.in>
2026-08-14 10:33:26 +0530

    Adding all codde

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://amankoli1206_db_user:Amankoli@12062006@cluster0escrow.cvwvhac.mongodb.net/?appName=Cluster0Escrow');
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
+
+db.on('error', (err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
+module.exports = db;
